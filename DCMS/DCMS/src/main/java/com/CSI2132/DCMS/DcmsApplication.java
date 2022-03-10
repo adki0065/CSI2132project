b4f4ca7ec4150d4628297bb2f8eec1340cf80f5c
adki0065 <jadki0065@uottawa.ca>
2022-03-09 21:08:43 -0500

    added springboot java app

---
+++ DCMS/DCMS/src/main/java/com/CSI2132/DCMS/DcmsApplication.java
@@ -0,0 +1,13 @@
+package com.CSI2132.DCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcmsApplication.class, args);
+	}
+
+}
---
+++ DCMS/DCMS/src/test/java/com/CSI2132/DCMS/DcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSI2132.DCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
